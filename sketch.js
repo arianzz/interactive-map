57106480e297df86da763f7a54a2941b3b1bb758
Ariana Vassilopoulou <arianavassilopoulou@172-16-241-202.DYNAPOOL.NYU.EDU>
2017-10-31 16:56:50 -0400

    pushing to the web

---
+++ sketch.js
@@ -0,0 +1,65 @@
+var myButton;
+
+var backButton;
+
+var img;
+
+function preload() {
+    img = loadImage("map.jpg");
+}
+
+function setup() {
+    //bg = loadImage("map.jpg");
+    createCanvas(800, 800); //??? do i need to create a canvas in this case?
+    image(img, 0, 0, 700, 700);
+
+
+    // image(img,0,0,1800,1800,0,1000);
+    myButton = createButton('');
+    myButton.style("border-radius", "50%");
+    myButton.style("background-color", "blue");
+    myButton.style("border", "none");
+    myButton.position(135, 400);
+    myButton.size(10, 10);
+    myButton.mousePressed(myButtonPressed);
+
+
+    backButton = createButton('Back');
+    backButton.mousePressed(backButtonPressed);
+    backButton.position(100, 100);
+    backButton.style("visibility", "hidden");
+
+
+
+    // zoom.position(img,0,0,1800,1800,0,1000);
+
+}
+
+function draw() {
+
+}
+
+function myButtonPressed() {
+    background(255);
+    document.getElementById("canvas").style.display = "block";
+    image(img, 0, 0, 1800, 1800, 0, 1000);
+    myButton.style("visibility", "hidden");
+    backButton.style("visibility", "visible");
+    magazine("visible");
+
+}
+
+function backButtonPressed() {
+    background(255);
+    document.getElementById("canvas").style.display = "none";
+    // image(img, 0, 0, 800, 800);
+    // image(img, 0, 0, 1800, 1800, 0, 1000);
+    image(img, 0, 0, 700, 700);
+    backButton.style("visibility", "hidden");
+    myButton.style("visibility", "visible");
+    magazine("hidden");
+}
+
+
+
+//button click and increase variable bg height and width
