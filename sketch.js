9e54c5684c9983afdf3f0c291c45aa609a7d5041
Ariana Vassilopoulou <arianavassilopoulou@172-16-242-113.DYNAPOOL.NYU.EDU>
2017-11-29 20:16:49 -0500

    alejandro is the best v3

--- sketch.js
+++ sketch.js
@@ -42,12 +42,12 @@ function draw() {
 function myButtonPressed() {
   console.log('my button pressed');
     background(255);
-    // document.getElementById("canvas").style.display = "block";
+    document.getElementById("canvas").style.display = "block";
     // image(img, 0, 0, 1800, 1800, 0, 1000);
     myButton.style("visibility", "hidden");
     backButton.style("visibility", "visible");
     backButton.style("z-index", "9999");
-    magazine("visible");
+    // magazine("visible");
     console.log(backButton);
 }
 
@@ -59,7 +59,7 @@ function backButtonPressed() {
     image(img, 0, 0, 1000, 700);
     backButton.style("visibility", "hidden");
     myButton.style("visibility", "visible");
-    magazine("hidden");
+    // magazine("hidden");
 }
 
 
